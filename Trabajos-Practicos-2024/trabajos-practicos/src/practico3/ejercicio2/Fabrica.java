9e05d3cbe2f654d31b70fd893a2bf9f7b8f8512b
Leonardo <leonardosanchez119@gmail.com>
2024-08-24 16:48:25 -0300

    ejercicio 2 del practico 3 realizado

--- Trabajos-Practicos-2024/trabajos-practicos/src/practico3/ejercicio2/Fabrica.java
+++ Trabajos-Practicos-2024/trabajos-practicos/src/practico3/ejercicio2/Fabrica.java
@@ -1,5 +1,28 @@
 package practico3.ejercicio2;
 
+import java.util.ArrayList;
+
 public class Fabrica {
-    
+    private ArrayList<Producto> productos;
+
+    public Fabrica(){
+        this.productos = new ArrayList<>();
+    }
+    public double precioDeTodosLosProductos(){
+        double aux=0;
+        for (Producto pp : productos) {
+           aux+=pp.getCostoFabricacion() + pp.getValorDeVenta(); 
+        }
+        return aux;
+    }
+
+    public ArrayList<Producto> getProductosDisponibles(){
+        ArrayList productosDisponibles = new ArrayList<>();
+        for (Producto pp : productos) {
+            if (pp.isVendido()) {
+                productosDisponibles.add(pp);
+            }
+        }
+        return productosDisponibles;
+    }
 }
--- Trabajos-Practicos-2024/trabajos-practicos/src/practico3/ejercicio2/Producto.java
+++ Trabajos-Practicos-2024/trabajos-practicos/src/practico3/ejercicio2/Producto.java
@@ -1,5 +1,70 @@
 package practico3.ejercicio2;
 
 public class Producto {
+    private boolean vendido;
+    private String tipo;
+    private double peso;
+    private double costoFabricacion;
+    private double valorVenta;
+    private String tipoDeMadera;
+    private String color;
+    private double tasaDeRecarga;
+
+    public Producto(boolean vendido, String tipo, double peso, double costoFabricacion, double valorVenta,
+            String tipoDeMadera, String color, double tasaDeRecarga) {
+        this.vendido = false;
+        this.tipo = tipo;
+        this.peso = peso;
+        this.costoFabricacion = costoFabricacion;
+        this.valorVenta = valorVenta;
+        this.tipoDeMadera = tipoDeMadera;
+        this.color = color;
+        this.tasaDeRecarga = 1.30;
+    }
+    public boolean isVendido() {
+        return vendido;
+    }
+    public void setVendido(boolean vendido) {
+        this.vendido = true;
+    }
+    public String getTipo() {
+        return tipo;
+    }
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+    public double getPeso() {
+        return peso;
+    }
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+    public String getTipoDeMadera() {
+        return tipoDeMadera;
+    }
+    public void setTipoDeMadera(String tipoDeMadera) {
+        this.tipoDeMadera = tipoDeMadera;
+    }
+    public String getColor() {
+        return color;
+    }
+    public void setColor(String color) {
+        this.color = color;
+    }
+    public double getCostoFabricacion() {
+        return costoFabricacion;
+    }
+    public void setCostoFabricacion(double costoFabricacion) {
+        this.costoFabricacion = costoFabricacion;
+    }
+    public void setValorVenta(double valorVenta) {
+        this.valorVenta = valorVenta;
+    }
+    public double getTasaDeRecarga(){
+        return this.tasaDeRecarga;
+    }
+    public double getValorDeVenta(){
+        return this.costoFabricacion()*this.getTasaDeRecarga();
+    }
     
 }
