29abcc4f6301c43a851addd63584179fe00beda5
Leonardo200298 <leonardosanchez119@gmail.com>
2024-08-25 16:44:49 -0300

    ejercicio 4 del tp 3 terminado

--- Trabajos-Practicos-2024/trabajos-practicos/src/practico3/ejercicio3/Jugar.java
+++ Trabajos-Practicos-2024/trabajos-practicos/src/practico3/ejercicio3/Jugar.java
@@ -1,18 +1,63 @@
 package practico3.ejercicio3;
 
-public class Jugar {
-    private Jugador j1;
-    private Jugador j2;
-    public Jugar(){
-        this.j1 = new Jugador("Heroe","Leonardo", "Caballero lechero");
-        this.j2 = new Jugador("Hechicero", "Messi", "Hechicero choricero");
-    }
+import java.util.ArrayList;
 
-    public void jugar(){
+public class Jugar {
+    private ArrayList<Jugador> jugadores;
+    private int jugadorQueQuiero;
+
+    public Jugar(int jugadorQueQuiero) {
+        this.jugadores = new ArrayList<>();
+        this.jugadorQueQuiero = jugadorQueQuiero;
+    }
+    public boolean puedoJugar(){
+        return jugadores.size()%2 == 0;
+    }
+    public ArrayList<Jugador> getJugadores(){
+        return this.jugadores;
+    }
+    public void setJugadorQueQuiero(int jj){
+        this.jugadorQueQuiero = jj;
+    }
+    public void elegirJugadores(){
+
+        ArrayList<Jugador> jugadoresDisponibles = new ArrayList<>();
+        if (puedoJugar()) {
+            jugadoresDisponibles = this.getJugadores();
+            eligeJugador(jugadoresDisponibles);
+        }
+    }
+    public void eligeJugador(ArrayList<Jugador> jugadoresDisponibles){
+
+        for (int i = 0; i < jugadoresDisponibles.size(); i++) {
+            if (this.jugadorQueQuiero > 0 && this.jugadorQueQuiero < jugadoresDisponibles.size()) {
+                Jugador j1 = jugadoresDisponibles.get(jugadorQueQuiero);
+                jugadoresDisponibles.remove(jugadorQueQuiero);
+                Jugador j2 = jugadoresDisponibles.get(jugadoresDisponibles.size()-1);
+                
+                jugar(j1,j2);
+            }
+        }
+    }
+    private void jugar(Jugador j1, Jugador j2) {
+        Jugador jj;
         for (Caracteristica cj1 : j1.getCaracteristicas()) {
             for (Caracteristica cj2 : j2.getCaracteristicas()) {
                 if (cj1.getAltura() > cj2.getAltura()) {
-                    System.out.println("Gano el jugador "+ j1.getNombreReal());
+                    jj = j1;
+                    System.out.println("El jugador ganador es" + jj.getNombreReal());
+                }else{
+                    jj = j2;
+                }
+                if (cj1.getAltura() == cj2.getAltura()) {
+                    if (cj1.getPeso() > cj2.getPeso()) {
+                        jj = j1;
+                        System.out.println("El jugador ganador es" + jj.getNombreReal());
+                    }else{
+                        jj = j2;
+                    }
+                    System.out.println("Volver a jugar hasta que enseñen Strategy");
+                    jugadores.clear();
                 }
             }
         }
