a89e7993e7fe87763de6e1c7e222b74ff04f74f3
Leonardo200298 <leonardosanchez119@gmail.com>
2024-08-26 13:39:57 -0300

    trabajo practico numero 4 terminado

--- Trabajos-Practicos-2024/trabajos-practicos/src/practico3/ejercicio4/Celular.java
+++ Trabajos-Practicos-2024/trabajos-practicos/src/practico3/ejercicio4/Celular.java
@@ -41,9 +41,37 @@ public class Celular {
     }
     public ArrayList<Contacto> contactosRepetidos(){
         ArrayList<Contacto> contactosRep = new ArrayList<>();
-        for (Contacto contacto : contactos) {
-            
+        for (int i = 0; i < contactos.size() - 1; i++) {
+            boolean encontrado = false;
+            for (int j = i+1; j < contactos.size() - 1; j++) {
+                if (cadenasIguales(contactos.get(i).getNombre(), contactos.get(j).getNombre())
+                &&(cadenasIguales(contactos.get(i).getApellido(),contactos.get(j).getApellido()))
+                &&(numerosIguales(contactos.get(i).getTelefono(), contactos.get(j).getTelefono()))) {
+                    encontrado = true;
+                }
+            }
+            if (encontrado && !contactosRep.contains(contactos.get(i))) {
+                contactosRep.add(contactos.get(i));
+                
+            }
         }
+      
         return contactosRep;
+    }
+    public ArrayList<Contacto> contactosConNumerosIguales(){
+        ArrayList<Contacto> numerosIgualess = new ArrayList<>();
+        for (int i = 0; i < this.contactosRepetidos().size(); i++) {
+            boolean encontrado = false;
+            for (int j = i+1; j < this.contactosRepetidos().size(); j++) {
+                if (numerosIguales(this.contactosRepetidos().get(i).getTelefono(),this.contactosRepetidos().get(j).getTelefono())) {
+                    encontrado = true;
+                    
+                }
+            }
+            if (encontrado && !numerosIgualess.contains(numerosIgualess.get(i))) {
+                numerosIgualess.add(this.contactosRepetidos().get(i));
+            }
+        }
+        return numerosIgualess;
     }
 }
