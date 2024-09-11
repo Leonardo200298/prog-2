17ca0fcadfc0012a2f58a70c72180336039ce49f
Leonardo200298 <leonardosanchez119@gmail.com>
2024-09-11 15:54:08 -0300

    agrego elemento

--- Trabajos-Practicos-2024/trabajos-practicos/src/practico6/ejercicio3/ColaDeEspera.java
+++ Trabajos-Practicos-2024/trabajos-practicos/src/practico6/ejercicio3/ColaDeEspera.java
@@ -1,4 +1,5 @@
 package practico6.ejercicio3;
+
 import java.util.ArrayList;
 
 public class ColaDeEspera {
@@ -10,5 +11,36 @@ public class ColaDeEspera {
         this.comparador = comparador;
     }
 
-    
+    public void agregarVehiculo(Vehiculo v1) {
+        int lugar = 0;
+        while (lugar < vehiculosEnEspera.size() && comparador.comparaVehiculos(vehiculosEnEspera.get(lugar), v1)) {
+            lugar++;
+        }
+        if (lugar < vehiculosEnEspera.size()) {
+            vehiculosEnEspera.add(lugar, v1);
+        } else {
+            vehiculosEnEspera.add(v1);
+        }
+    }
+
+    public boolean tieneVehiculos() {
+        return this.vehiculosEnEspera.size() > 0;
+    }
+
+    public Vehiculo getPrimera() {
+        if (vehiculosEnEspera.size() > 0) {
+            Vehiculo t0 = vehiculosEnEspera.get(0);
+            vehiculosEnEspera.remove(0);
+            return t0;
+        } else {
+            return null;
+        }
+    }
+
+    public void copiarA(ColaDeEspera cc) {
+        while (this.tieneVehiculos()) {
+            cc.agregarVehiculo(this.getPrimera());
+        }
+    }
+
 }
