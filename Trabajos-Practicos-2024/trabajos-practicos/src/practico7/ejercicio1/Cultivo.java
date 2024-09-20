6e18417c123c7afea0f02a3f61f18c1fb333a93c
Leonardo200298 <leonardosanchez119@gmail.com>
2024-09-20 18:42:22 -0300

    falta terminar primer servicio pero deje un return false provisorio

--- Trabajos-Practicos-2024/trabajos-practicos/src/practico7/ejercicio1/Cultivo.java
+++ Trabajos-Practicos-2024/trabajos-practicos/src/practico7/ejercicio1/Cultivo.java
@@ -4,4 +4,14 @@ import java.util.ArrayList;
 public class Cultivo {
     private String nombre;
     private ArrayList<Enfermedad> enfermedades;
+
+    public Cultivo(String nombre){
+        this.nombre = nombre;
+        this.enfermedades = new ArrayList<>();
+    }
+
+    public boolean puedeTratar(ProductoQuimico pp){
+        //provisorio
+        return false;
+    }
 }
